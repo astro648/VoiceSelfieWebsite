54e1f3467a17122dc988aa384d40e89e9eaa9ca2
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-23 10:49:10 -0800

    debug and display

--- main.js
+++ main.js
@@ -6,4 +6,7 @@ function start(){
 }
 recognition.onresult = function(event){
     console.log(event)
+    var content = event.results[0][0].transcript;
+    console.log(content)
+    document.getElementById("textbox").innerHTML=content;
 }
